30a9efaf352a77ad633d375670f3e653c8cd4d03
Tim Dietrich <timdietrich@gmail.com>
2018-10-19 13:12:42 -0700

    Add private routes

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+# switch node version
+nvm install v8.9.4
+nvm use v8.9.4
+
+# install yarn
+
+
 # Git Commands
 
 git init - Create a new git repo
@@ -5,3 +12,16 @@ git status - View the changes to your project code
 git add - Add files to staging area
 git commit - Creates a new commit with files from staging area
 git log - View recent commits
+
+# Run Developement Server
+yarn run dev-server
+
+# Run test suite
+yarn test
+yarn test -- --watch
+
+# Commit
+git push
+
+# Deploy to Heroku
+git push heroku master
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('render Header', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {}} />);
     expect(wrapper).toMatchSnapshot();
 
     // expect(wrapper.find('h1').text()).toBe('Expensify');
@@ -13,4 +13,11 @@ test('render Header', () => {
     // expect(renderer.getRenderOutput()).toMatchSnapshot();
 
     //console.log(renderer.getRenderOutput());
+});
+
+test('startLogout on button click', () => {
+    const startLogout = jest.fn(); // spy
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
 });
